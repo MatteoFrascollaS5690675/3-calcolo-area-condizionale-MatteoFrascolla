51776d52becd8e0644cce876651775dba477ac10
MatteoFrascollaS5690675 <118465812+MatteoFrascollaS5690675@users.noreply.github.com>
2022-11-18 12:36:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   int opzione=0;
+    float a=0 , b=0;
+    cin>>a>>b;
+    cin>>opzione;
+    switch(opzione){
+        case 0: cout<<(a*b)/2<<endl;
+        break;
+        case 1: cout<<(a*a)<<endl;
+        break;
+        case 2: cout<<(a*b)<<endl;
+        break;
+        defaul: cout<<"opzione non valida"<<endl;
+    }
    return 0;
 }
